7f9fd58f7132293e0004dad5323a623d872bc0cd
sk0814629 <sk0814629@gmail.com>
2025-04-29 12:45:06 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let mobile_menu = document.getElementById('mobile-menu');
+let mobile_menubar = document.getElementById('mobile-menubar');
+let close = document.getElementById('close');
+let mobile_menu_close = document.getElementById('mobile-menu-close'); // Correct method here
+
+mobile_menu.addEventListener('click', () => {
+    // alert("everything");
+    mobile_menubar.classList.toggle('show'); 
+});
+
+mobile_menu_close.addEventListener('click', () => {
+    mobile_menubar.classList.remove('show'); 
+});
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
