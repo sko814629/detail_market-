e8dd975012d939e67bed555af935d461ff9ab22c
sk0814629 <sk0814629@gmail.com>
2025-05-06 10:08:45 +0530

    resolved in mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let mobile_menu = document.getElementById('mobile-menu');
 let mobile_menubar = document.getElementById('mobile-menubar');
 let close = document.getElementById('close');
-let mobile_menu_close = document.getElementById('mobile-menu-close'); // Correct method here
-
+let mobile_menu_close = document.getElementById('mobile-menu-close'); 
+ 
 mobile_menu.addEventListener('click', () => {
     mobile_menubar.classList.toggle('show'); 
 });
@@ -10,6 +10,7 @@ mobile_menu.addEventListener('click', () => {
 mobile_menu_close.addEventListener('click', () => {
     mobile_menubar.classList.remove('show'); 
 });
+
 // var swiper = new Swiper(".mySwiper", {
 //     slidesPerView: "auto",
 //     spaceBetween: 30,
